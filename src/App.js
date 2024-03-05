1bea5c8ca404fbb3fbdb320b6cd3e8235804f40b
scamgi <scampini.giacomo@gmail.com>
2024-03-05 21:25:28 +0100

    Auth.js added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import { useState, useRef } from 'react';
 import './App.css';
+import { Auth } from './components/Auth.js';
+
+// we use cookies to know if the user is logged in
+import Coockies from 'universal-cookie';
+const cookies = new Coockies();
 
 function App() {
-  return <div>Hello world.</div>;
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [room, setRoom] = useState("");
+
+  const roomInputRef = useRef(null);
+
+  if (!isAuth) {
+    return <div>
+      <Auth setIsAuth={setIsAuth} />
+    </div>;
+  }
+
+  return (
+    <div>
+      {room ? (
+        <div>Chat</div> // UI
+      ) : (
+        <div className='room'>
+          <label>Enter Room Name:</label>
+          <input ref={roomInputRef} />
+          <button
+            onClick={() => setRoom(roomInputRef.current.value)}>
+            Enter Chat
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Auth.js
@@ -0,0 +1,29 @@
+import { auth, provider } from '../config/firebase-config';
+import { signInWithPopup } from 'firebase/auth';
+
+import Coockies from 'universal-cookie';
+const cookies = new Coockies();
+
+export const Auth = (props) => {
+    const { setIsAuth } = props;
+
+    const signInWithGoogle = async () => {
+        try {
+            const result = await signInWithPopup(auth, provider);
+            cookies.set('auth-token', result.user.refreshToken);
+            // we set setIsAuth = true here because I don't need 
+            // to refresh in order to go from the starting page to the other one
+            setIsAuth = true;
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <div className="auth">
+            <p>Sign In With Google To Continue</p>
+            <button onClick={signInWithGoogle}>Sign In With Google</button>
+        </div>
+    )
+}
